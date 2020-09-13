9afe845c496b4da0df81bfc0bcfa7daf84b7fea2
Sergey <siniiitsa@gmail.com>
2020-09-13 23:10:21 +0300

    refactoring

--- scripts/confirm.js
+++ scripts/confirm.js
@@ -1,23 +1,32 @@
-export const asyncConfirm = (question, options) => {
-  const shadow = document.createElement('div');
-  shadow.classList.add('shadow');
+const toButton = ({ label, value }) => (
+  `<button data-type="option" data-value="${value}" class="option">
+    ${label}
+  </button>`
+);
+
+const buildButtonsHTML = (options) => (
+  options.map(toButton).join('')
+);
+
+const buildModal = (question, options) => {
   const modal = document.createElement('div');
   modal.classList.add('modal');
-  shadow.append(modal);
-
-  const buttons = options.map(({ label, value }) => (
-    `<button data-type="option" data-value="${value}" class="option">${label}</button>`
-  ));
-
-  const content = `
+  modal.innerHTML = `
     <h3 class="question">${question}</h3>
     <div class="buttons">
-      ${buttons.join('')}
+      ${buildButtonsHTML(options)}
     </div>
   `;
 
-  modal.innerHTML = content;
-  document.body.append(shadow);
+  return modal;
+}
+
+export const asyncConfirm = (question, options) => {
+  const shadow = document.createElement('div');
+  shadow.classList.add('shadow');
+  const modal = buildModal(question, options);
+  shadow.append(modal);
+  document.body.prepend(shadow);
 
   return new Promise((resolve) => {
     modal.addEventListener('click', (e) => {
@@ -25,7 +34,7 @@ export const asyncConfirm = (question, options) => {
         return;
       };
 
-      e.currentTarget.parentElement.remove();
+      shadow.remove();
       const { value } = e.target.dataset;
       resolve(value);
     });
